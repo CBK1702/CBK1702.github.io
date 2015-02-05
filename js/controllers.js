33fa7444f70c30699c190f474472785f9f2c6513
CBK1702 <stylesclashextreme@gmail.com>
2015-02-05 12:03:20 +0000

    added video

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,20 @@
-angular.module('starter.controllers', [])
+angular.module('LoadingApp', ['ionic'])
+.controller('LoadingCtrl', function($scope, $ionicLoading) {
+  $scope.show = function() {
+    $ionicLoading.show({
+      template: 'Loading...'
+    });
+  };
+  $scope.hide = function(){
+    $ionicLoading.hide();
+  };
+});
+
+
+
+
+
+
+
 
     
