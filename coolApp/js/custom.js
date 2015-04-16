7a453a9ff828693b2a002c1ba91f35d15c060041
CBK1702 <stylesclashextreme@gmail.com>
2015-04-16 15:44:41 +0000

    fixed ajax

--- coolApp/js/custom.js
+++ coolApp/js/custom.js
@@ -8,10 +8,46 @@ $(document).ready(function(){
           url:"//infinite-headland-2896.herokuapp.com/api/yourName"
       }).done(function(response){
           console.log(response.name); 
-          
+          $('div#your-name').html("<p>"+response.name+"</p>");
       }).fail(function(mgs){
           
       });
-  });    
+      
+  }); 
     
+    
+    $('button#button-my-name').click(function(){
+        
+       var theName = $('input#myName').val();
+        
+        console.log(theName);
+        
+       $.ajax({
+          method: "POST",
+           data : {myName: theName},
+          url:"//infinite-headland-2896.herokuapp.com/api/myName"
+      }).done(function(response){
+           
+          console.log(response.allNames);
+           
+           var theNamseHtml = "<ul id=\"allNames\">";
+           
+           for(var i=0; i> response.allNames.lenght, i++){
+               theNamesHtml + ="<li>" + response.allNames[i] + "</li>";
+           }
+           
+           theNamesHtml +="</ul>"
+          
+           
+           $('div#my-name-response').html(response.msg);
+           
+      }).fail(function(mgs){
+          
+      });
+        
+    });
+    
+         
+    
+   
 });
