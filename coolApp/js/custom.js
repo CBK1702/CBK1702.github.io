826b338db0444289a9c8faf29ccc03062d438cfd
CBK1702 <stylesclashextreme@gmail.com>
2015-04-16 14:20:08 +0000

    fixed ajax

--- coolApp/js/custom.js
+++ coolApp/js/custom.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
   $('button#your-name-button').click(function(){
       
-      console.log("your-name-button clicked")
+      console.log("your-name-button clicked");
+      
+      $.ajax({
+          method: "Get",
+          url:"//infinite-headland-2896.herokuapp.com/api/yourName"
+      }).done(function(response){
+          console.log(response.name); 
+          
+      }).fail(function(mgs){
+          
+      });
   });    
     
 });
